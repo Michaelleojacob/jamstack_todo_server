c9406f194ffc98e2d03ded18d0ed2f885bf88490
mig <michaelleojacob@gmail.com>
2022-12-25 22:14:38 -0800

    updated db2/user.ts I no longer pass the hash or password in user queries by using prisma select(s)

--- db2/users.ts
+++ db2/users.ts
@@ -1,26 +1,33 @@
 import prisma from "../config/db";
 
 // returns all users
-const findUsers = async () => await prisma.user.findMany({});
+const findUsers = async () =>
+  await prisma.user.findMany({ select: { id: true, username: true } });
 // findUsers().then((res) => console.log(res));
 
 // returns user: {}, or null
 const findUser = async (id: number) =>
-  await prisma.user.findUnique({ where: { id } });
-// findUser(14).then((res) => console.log(res));
+  await prisma.user.findUnique({
+    where: { id },
+    select: { id: true, username: true },
+  });
+// findUser(15).then((res) => console.log(res));
 // findUser(200).then((res) => console.log(res));
 
-/**
- * create user
- * or if username is taken:
- * return null
- */
+const userExists = async (id: number) =>
+  !!(await prisma.user.findUnique({ where: { id } }));
+// userExists(15).then((res) => console.log(res));
+
+// return true or null
 const createUser = async (username: string, password: string) => {
   try {
-    return await prisma.user.create({ data: { username, password } });
+    const user = await prisma.user.create({ data: { username, password } });
+    return { username: user.username, id: user.id };
   } catch (e) {
     console.log(`err in createUser`, e);
     return null;
   }
 };
 // createUser("123", "lol").then((res) => console.log(res));
+
+export { findUsers, findUser, userExists, createUser };
