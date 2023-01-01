73aa4fa66506ec6a702daefbcfee18c1aa545427
mig <michaelleojacob@gmail.com>
2022-12-31 19:36:12 -0800

    removed redudant export on createToken

--- utils/auth/createToken.ts
+++ utils/auth/createToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { UserInfo } from "../../types/types";
 
-export const createToken = (user: UserInfo) =>
+const createToken = (user: UserInfo) =>
   jwt.sign(user, process.env.TOKEN_SECRET!, { expiresIn: "6h" });
 
 export default createToken;
