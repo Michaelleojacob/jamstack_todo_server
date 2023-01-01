528c64012cc2df2bb08f1ab47f2fef7b920fa8d0
mig <michaelleojacob@gmail.com>
2022-12-31 19:54:08 -0800

    starting from a clean slate for todo and project routes so I can rework all the logic to be in a controller

--- config/router.ts
+++ config/router.ts
@@ -1,8 +1,6 @@
 import { Application } from "express-serve-static-core";
 import authRouter from "../routes/auth/auth";
 import protectedRouter from "../routes/protected/protected";
-import todoRouter from "../routes/todos/todos";
-import projectRouter from "../routes/projects/projects";
 
 const routerConfig = (app: Application) => {
   app.use("/auth", authRouter);
