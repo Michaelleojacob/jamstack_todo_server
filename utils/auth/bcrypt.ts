7be6e418b96a834c7970e49d83c1033232ceb6ca
mig <michaelleojacob@gmail.com>
2023-01-01 20:37:32 -0800

    changed export objects to export each function individually. Export defaults remain the same. IE -> export {f1, f2, f3} -> export const f1 = () =>, export const f2 = () =>

--- utils/auth/bcrypt.ts
+++ utils/auth/bcrypt.ts
@@ -1,12 +1,10 @@
 import bcrypt from "bcrypt";
 
-const hashPassword = async (str: string) => {
+export const hashPassword = async (str: string) => {
   const salt = bcrypt.genSaltSync(+process.env.SALT!);
   return bcrypt.hashSync(str, salt);
 };
 
-const comparePassword = async (str: string, hash: string) => {
+export const comparePassword = async (str: string, hash: string) => {
   return bcrypt.compareSync(str, hash);
 };
-
-export { hashPassword, comparePassword };
