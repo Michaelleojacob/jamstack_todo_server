6e68bde4071e4a6f13def9dd882cac2233974889
mig <michaelleojacob@gmail.com>
2023-06-17 22:32:22 -0700

    updated cors to work with deployed domains

--- config/cors.ts
+++ config/cors.ts
@@ -1,6 +1,13 @@
 import cors from "cors";
 
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://todoclient-self.vercel.app",
+  "https://todoclient-self.vercel.app/",
+  "https://todoclient-git-main-michaelleojacob.vercel.app",
+  "https://todoclient-git-main-michaelleojacob.vercel.app/",
+];
 
 const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
